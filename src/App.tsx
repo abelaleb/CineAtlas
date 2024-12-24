c4e6e2219aaf8ea31ea6ab66f5fbe45ee70fee60
abelaleb <abelaleb@users.noreply.github.com>
2024-12-24 22:28:37 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos saepe
+      <span className="text-white bg-red">
+        {' '}
+        voluptatum quam sint veritatis debitis, consequatur fuga accusamus eius
+      </span>
+      magnam molestiae architecto sequi reprehenderit amet odio, natus earum
+      numquam qui!
+    </div>
+  );
+}
+
+export default App;
