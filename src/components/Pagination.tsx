6d08c7e7251de99dfed43632469262984405fa42
abelaleb <abelaleb@users.noreply.github.com>
2025-01-02 03:37:19 +0300

    fix: navigation with pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,3 +1,5 @@
+import { ArrowRight, ArrowLeft } from 'lucide-react';
+
 interface PaginationProps {
   totalPosts: number;
   postsPerPage: number;
@@ -11,27 +13,60 @@ const Pagination = ({
   setCurrentPage,
   currentPage,
 }: PaginationProps) => {
+  const totalPages = Math.ceil(totalPosts / postsPerPage);
   const pages = [];
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+  for (let i = 1; i <= totalPages; i++) {
     pages.push(i);
   }
+
   return (
     <div className="flex flex-wrap justify-center mt-4">
-      {pages.map((page, index) => {
-        return (
+      {currentPage > 1 && (
         <button
-            key={index}
-            onClick={() => setCurrentPage(page)}
-            className={`w-10 h-10 font-semibold text-base mx-[10px] rounded-[6px] px-4 py-2 m-2 transition-colors duration-300 cursor-pointer ${
-                page === currentPage
-                    ? 'active bg-blue-500 border-blue-500'
-                    : 'bg-gray-200 border-gray-200'
-            }`}
+          onClick={() => setCurrentPage(currentPage - 1)}
+          className="w-10 h-10 font-semibold  mx-[10px] rounded-[6px] p-3 m-2 transition-colors duration-300 cursor-pointer bg-[#8A49A6] border-gray-200 text-white flex justify-center items-center"
         >
-            {page}
+          <ArrowLeft />
         </button>
-        );
-      })}
+      )}
+      {currentPage > 1 && (
+        <button
+          onClick={() => setCurrentPage(currentPage - 1)}
+          className={`w-10 h-10 font-semibold text-base mx-[10px] rounded-[6px] px-4 py-2 m-2 transition-colors duration-300 cursor-pointer ${
+            currentPage - 1 === currentPage
+              ? 'active bg-[#1C0326] border-[#1C0326] text-white'
+              : 'bg-[#8A49A6] border-gray-200 text-white'
+          }`}
+        >
+          {currentPage - 1}
+        </button>
+      )}
+      <button
+        onClick={() => setCurrentPage(currentPage)}
+        className="w-10 h-10 font-semibold text-base mx-[10px] rounded-[6px] px-4 py-2 m-2 transition-colors duration-300 cursor-pointer active bg-[#1C0326] border-[#1C0326] text-white"
+      >
+        {currentPage}
+      </button>
+      {currentPage < totalPages && (
+        <button
+          onClick={() => setCurrentPage(currentPage + 1)}
+          className={`w-10 h-10 font-semibold text-base mx-[10px] rounded-[6px] px-4 py-2 m-2 transition-colors duration-300 cursor-pointer ${
+            currentPage + 1 === currentPage
+              ? 'active bg-[#1C0326] border-[#1C0326] text-white'
+              : 'bg-[#8A49A6] border-gray-200 text-white'
+          }`}
+        >
+          {currentPage + 1}
+        </button>
+      )}
+      {currentPage < totalPages && (
+        <button
+          onClick={() => setCurrentPage(currentPage + 1)}
+          className="w-10 h-10 font-semibold text-base mx-[10px] rounded-[6px] p-3 m-2 transition-colors duration-300 cursor-pointer bg-[#8A49A6] border-gray-200 text-white flex justify-center items-center"
+        >
+          <ArrowRight />
+        </button>
+      )}
     </div>
   );
 };
