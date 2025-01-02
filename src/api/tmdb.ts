5612967bae0e0e2e02b337909d750fe7e39f2e99
abelaleb <abelaleb@users.noreply.github.com>
2025-01-02 03:31:54 +0300

    fix: minor bugs tmdb.ts

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -14,32 +14,56 @@ const tmdb = axios.create({
 });
 
 export interface MovieChange {
+  adult: false;
+  backdrop_path: string;
+  genre_ids: Array<number>;
   id: number;
-  adult: boolean;
-  original_title: string;
-  original_name: string;
+  media_type: string;
   original_language: string;
-  release_date: string;
-  title: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
   poster_path: string;
+  release_date: string | number | Date;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }
 
 export interface TVShowChange {
-  id: number;
   adult: boolean;
+  backdrop_path: string;
+  first_air_date: string | number | Date;
+  genre_ids: Array<number>;
+  id: number;
+  media_type: string;
+  name: string;
+  origin_country: Array<string>;
+  original_language: string;
   original_name: string;
+  overview: string;
+  popularity: number;
   poster_path: string;
+  vote_average: number;
+  vote_count: number;
 }
 
 export interface PersonChange {
+  adult: boolean;
+  gender: 0|2;
   id: number;
+  known_for_department: string;
+  media_type: string;
   name: string;
-  profile_path: string;
   original_name: string;
-  original_title: string;
+  popularity: number;
+  profile_path: null;
 }
 export interface searchChange {
-  backdrop_path: any;
+  original_language: string;
+  vote_average: number;
+  backdrop_path: string;
   id: number;
   adult: boolean;
   original_title: string;
@@ -56,43 +80,51 @@ export interface PaginatedResponse<T> {
 }
 
 //Fetch data for Movies
-export const getMovieChanges = async (
+export const getTrendingMovies = async (
   page = 1
 ): Promise<PaginatedResponse<MovieChange>> => {
-  // const response = await tmdb.get(`/movie/changes?page=1`, {
-  const response = await tmdb.get(`trending/movie/day?language=en-US// `, {
+  const response = await tmdb.get(`trending/movie/day?language=en-US `, {
     params: { page },
   });
+  console.log('Trending Movies: ', response.data.results);
   return response.data;
 };
 
 //Fetch data for TV Shows
-export const getTVChanges = async (
+export const getTrendingTvShows = async (
   page = 1
 ): Promise<PaginatedResponse<TVShowChange>> => {
   const response = await tmdb.get(
     `https://api.themoviedb.org/3/trending/tv/day?language=en-US`,
     { params: { page } }
   );
+  console.log('Trending tvShows: ', response.data.results);
   return response.data;
 };
 
 //Fetch data for People
-export const getPersonChanges = async (
+export const getTrendingPeople = async (
   page = 1
 ): Promise<PaginatedResponse<PersonChange>> => {
   const response = await tmdb.get(
     `https://api.themoviedb.org/3/trending/person/day?language=en-US`,
     { params: { page } }
   );
+  console.log('Trending people: ', response.data.results);
   return response.data;
 };
 
-//Fetch data for Search
-// const all = `https://api.themoviedb.org/3/search/multi?include_adult=false&language=en-US&page=1`;
-// const movie = `https://api.themoviedb.org/3/search/movie?include_adult=true&language=en-US&page=1`;
-// const tv = `https://api.themoviedb.org/3/search/tv?include_adult=true&language=en-US&page=1`;
-// const person = `https://api.themoviedb.org/3/search/person?include_adult=true&language=en-US&page=1`;
+// Fetch data for all trending items
+export const getTrendingAll = async (
+  page = 1
+): Promise<PaginatedResponse<searchChange>> => {
+  const response = await tmdb.get(`trending/all/day?language=en-US`, {
+    params: { page },
+  });
+  console.log('Trending All: ', response.data.results);
+  return response.data;
+};
+
 export const search = async (
   query: string,
   page = 1,
@@ -102,17 +134,7 @@ export const search = async (
   const response = await tmdb.get(endpoint, {
     params: { query, page, include_adult: false },
   });
-  console.log(response.data.results);
-
-  return response.data;
-  // const endpoint = type ? `https://api.themoviedb.org/3/search/movie?include_adult=true&language=en-US&page=1`
-  // // const response = await tmdb.get(
-  // //   `https://api.themoviedb.org/3/search/multi?include_adult=false&language=en-US&page=1`,
-  // //   {
-  // //     params: { query, page },
-  // //   }
-  // // );
-  console.log(response.data.results);
+  console.log("search response",response.data.results);
 
   return response.data;
 };
