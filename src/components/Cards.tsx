126c35d79fad527b02494802fa50e22a66f94a92
abelaleb <abelaleb@users.noreply.github.com>
2025-01-02 03:32:27 +0300

    style: add card ui for cards

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,5 +1,9 @@
 import { image200 } from '@/Constants/Constants';
 import { MovieChange, TVShowChange, PersonChange } from '@/api/tmdb';
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
+import { ImageIcon, Star } from 'lucide-react';
+import { format } from 'date-fns/format';
+
 
 interface MovieCardProps {
   movies: MovieChange[];
@@ -13,27 +17,51 @@ interface PeopleCardProps {
 
 const MovieCards = ({ movies }: MovieCardProps) => {
   return (
-    <div className="flex flex-wrap gap-4 p-4">
+    <div className="flex flex-wrap gap-8 p-4 justify-center">
       {movies?.map((movie) => (
-        <div
-          // onClick={() => {
-          //   // navigate(`/${type}/` + movie.id);
-          // }}
+        <Card
           key={movie.id}
-          className="w-[calc(20%-1rem)] rounded-lg border cursor-pointer"
+          className="hover:shadow-lg p-0 h-[390px] w-[210px]"
         >
-          <img
-            src={image200 + movie.poster_path}
-            className="w-full rounded-t-lg"
-          />
-          {
-            <div className="p-2">
-              <h1 className="font-medium truncate text-sm line-clamp-2">
-                {movie.original_title || movie.original_name}
-              </h1>
+          <CardHeader className="p-0">
+            <CardTitle className="p-0 relative group">
+              <img
+                src={movie.poster_path ? image200 + movie.poster_path : ''}
+                onError={(e) => (e.currentTarget.style.display = 'none')}
+                className="w-[100%] h-[100%] max-h-[350px] overflow-hidden relative rounded-t-xl p-1"
+              />
+              {!movie.poster_path && (
+                <div className="flex items-center justify-center h-[307px] bg-gray-200 rounded-t-xl">
+                  <ImageIcon className="w-16 h-16 text-gray-400" />
+                </div>
+              )}
+              <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center gap-4 opacity-0 group-hover:opacity-100 hover:text-gray-500 hover:cursor-pointer transition-opacity duration-300 rounded-t-lg">
+                <div className="text-white text-lg font-bold self-center flex flex-col items-center">
+                  <div>
+                    <Star />
+                  </div>
+                  {Number.isInteger(movie.vote_average)
+                    ? movie.vote_average
+                    : movie.vote_average.toFixed(1)}
+                  /10
+                </div>
+                <div className="text-black text-sm bg-white bg-opacity-50 p-4 rounded-lg">
+                  {movie.original_language}
+                </div>
+              </div>
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="text-lg font-bold overflow-hidden text-ellipsis whitespace-nowrap hover:text-gray-500 hover:cursor-pointer">
+              {movie.original_title}
             </div>
-          }
-        </div>
+            <div>
+              {movie.release_date && (
+                <div>{format(new Date(movie.release_date), ' yyyy')}</div>
+              )}
+            </div>
+          </CardContent>
+        </Card>
       ))}
     </div>
   );
@@ -41,48 +69,99 @@ const MovieCards = ({ movies }: MovieCardProps) => {
 
 const TVShowCards = ({ tvShows }: TVShowCardProps) => {
   return (
-    <div className="flex flex-wrap gap-4 p-4">
+    <div className="flex flex-wrap gap-8 p-4 justify-center">
       {tvShows?.map((tvShow) => (
-        <div
+        <Card
           key={tvShow.id}
-          className="w-[calc(20%-1rem)] rounded-lg border cursor-pointer"
+          className="hover:shadow-lg p-0 h-[390px] w-[210px]"
         >
-          <img
-            src={image200 + tvShow.poster_path}
-            className="w-full rounded-t-lg"
-          />
-          {
-            <div className="p-2">
-              <h1 className="font-medium truncate text-sm line-clamp-2">
-                {tvShow.original_name}
-              </h1>
+          <CardHeader className="p-0">
+            <CardTitle className="p-0 relative group">
+              <img
+                src={tvShow.poster_path ? image200 + tvShow.poster_path : ''}
+                onError={(e) => (e.currentTarget.style.display = 'none')}
+                className="w-[100%] h-[100%] max-h-[350px] overflow-hidden relative rounded-t-xl p-1"
+              />
+              {!tvShow.poster_path && (
+                <div className="flex items-center justify-center h-[307px] bg-gray-200 rounded-t-xl">
+                  <ImageIcon className="w-16 h-16 text-gray-400" />
+                </div>
+              )}
+              <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center gap-4 opacity-0 group-hover:opacity-100 hover:text-gray-500 hover:cursor-pointer transition-opacity duration-300 rounded-t-lg">
+                <div className="text-white text-lg font-bold self-center flex flex-col items-center">
+                  <div>
+                    <Star />
+                  </div>
+                  {Number.isInteger(tvShow.vote_average)
+                    ? tvShow.vote_average
+                    : tvShow.vote_average.toFixed(1)}
+                  /10
+                </div>
+                <div className="text-black text-sm bg-white bg-opacity-50 p-4 rounded-lg">
+                  {tvShow.original_language}
+                </div>
+              </div>
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="text-lg font-bold overflow-hidden text-ellipsis whitespace-nowrap hover:text-gray-500 hover:cursor-pointer">
+              {tvShow.original_name}
             </div>
-          }
-        </div>
+            <div>
+              {tvShow.first_air_date && (
+                <div>{format(new Date(tvShow.first_air_date), ' yyyy')}</div>
+              )}
+            </div>
+          </CardContent>
+        </Card>
       ))}
     </div>
   );
 };
+
 const PeopleCards = ({ people }: PeopleCardProps) => {
   return (
-    <div className="flex flex-wrap gap-4 p-4">
+    <div className="flex flex-wrap gap-8 p-4 justify-center">
       {people?.map((person) => (
-        <div
+        <Card
           key={person.id}
-          className="w-[calc(20%-1rem)] rounded-lg border cursor-pointer"
+          className="hover:shadow-lg p-0 h-[390px] w-[210px]"
         >
-          <img
-            src={image200 + person.profile_path}
-            className="w-full rounded-t-lg"
-          />
-          <div className="p-2">
-            <h1 className="font-medium truncate text-sm line-clamp-2">
-              {person.name}
-            </h1>
-          </div>
-        </div>
+          <CardHeader className="p-0">
+            <CardTitle className="p-0 relative group">
+              <img
+                src={person.profile_path ? image200 + person.profile_path : ''}
+                onError={(e) => (e.currentTarget.style.display = 'none')}
+                className="w-[100%] h-[100%] max-h-[350px] overflow-hidden relative rounded-t-xl p-1"
+              />
+              {!person.profile_path && (
+                <div className="flex items-center justify-center h-[307px] bg-gray-200 rounded-t-xl p-1">
+                  <ImageIcon className="w-16 h-16 max-h-[350px] text-gray-400" />
+                </div>
+              )}
+              <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center gap-4 opacity-0 group-hover:opacity-100 hover:text-gray-500 hover:cursor-pointer transition-opacity duration-300 rounded-t-lg">
+                <div className="text-white text-lg font-bold self-center flex flex-col items-center">
+                  <div>
+                    <Star />
+                  </div>
+                  {person.known_for_department}
+                </div>
+                <div className="text-black text-sm bg-white bg-opacity-50 p-4 rounded-lg">
+                  {person.gender}
+                </div>
+              </div>
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="text-lg font-bold overflow-hidden text-ellipsis whitespace-nowrap hover:text-gray-500 hover:cursor-pointer">
+              {person.original_name}
+            </div>
+            <div>{person.media_type}</div>
+          </CardContent>
+        </Card>
       ))}
     </div>
   );
 };
+
 export { MovieCards, TVShowCards, PeopleCards };
